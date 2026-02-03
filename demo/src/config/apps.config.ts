40cf10350a3db3326efb9d27caf71a061c31137c
naninfinite <215418892+naninfinite@users.noreply.github.com>
2026-02-03 07:01:01 +0000

    removed terminal desktop icon

--- demo/src/config/apps.config.ts
+++ demo/src/config/apps.config.ts
@@ -16,8 +16,8 @@ export const appsConfig = new AppsConfig({
 			.setShowDesktopIcon(true),
 		terminal.setName("Commands")
 			.setDescription("A command line tool inspired by the Unix shell that runs entirely in your browser using ProzillaOS. Allows you to interact and manipulate the virtual drive and run silly commands.")
-			.setIconUrl("/assets/apps/icons/terminal.svg")
-			.setShowDesktopIcon(true),
+			.setIconUrl("/assets/apps/icons/terminal.svg"),
+			//.setShowDesktopIcon(true),
 		textEditor.setName("Notes")
 			.setDescription("Text editor for reading and writing text documents in a virtual file system using ProzillaOS.")
 			.setIconUrl("/assets/apps/icons/text-editor.svg"),
